88e9f8fa041dd3c201027b17001c635767f04ca5
sohan giri <sgiri7598@gmail.com>
2024-04-13 10:46:55 +0530

    working on contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,18 @@ import React from 'react'
 function Contact() {
   return (
     <div>
-      <h1>contact us</h1>
+    
+    <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235848.1033279115!2d87.72730677812501!3d22.536929300000025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a029f1faf4b3b47%3A0x1da1875f0c0546a5!2sChiranabin%20NGO!5e0!3m2!1sen!2sin!4v1712955413934!5m2!1sen!2sin"
+          width="500"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+
+      
     </div>
   )
 }
